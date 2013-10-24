13c0ffeeb6e4c7dd10eb4d50436f0caacc54abbf
bohua <fantasybohua@gmail.com>
2013-10-24 12:02:06 +0200

    import

---
+++ xlsMerger/MainWindow.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace XlsMerger
+{
+    public partial class MainWindow : Form
+    {
+        private SheetReader sheetReader = new SheetReader();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            /*
+            sheetReader.init(@"Xls\上海工具公司11390_20.xls");
+            DataTable dt = sheetReader.ConvertToDataTable();
+            dataSet1.Tables.Add(dt);
+
+            dataGridView1.DataSource = dataSet1.Tables[0];
+            for (int i = 0; i < dataGridView1.Columns.Count; i++)
+            {
+                dataGridView1.Columns[i].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            }*/
+        }
+    }
+}
---
+++ xlsMerger/SheetReader.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+
+namespace XlsMerger
+{
+    class SheetReader
+    {
+        private IWorkbook myWorkbook;
+
+        public void init(string path){
+            using (FileStream file = new FileStream(path, FileMode.Open, FileAccess.Read))
+            {
+                myWorkbook = new HSSFWorkbook(file);
+            }
+        }
+
+        public DataTable ConvertToDataTable()
+        {
+            ISheet sheet = myWorkbook.GetSheetAt(0);
+            System.Collections.IEnumerator rows = sheet.GetRowEnumerator();
+
+      
+            DataTable dt = new DataTable();
+
+            rows.MoveNext();
+            IRow headerRow = (HSSFRow)rows.Current;
+            for (int j = 0; j < headerRow.LastCellNum; j++)
+            {
+                ICell header = headerRow.GetCell(j);
+                dt.Columns.Add(header.ToString());
+            }
+
+            
+            while (rows.MoveNext())
+            {
+                IRow row = (HSSFRow)rows.Current;
+                DataRow dr = dt.NewRow();
+
+                for (int i = 0; i < row.LastCellNum; i++)
+                {
+                    ICell cell = row.GetCell(i);
+
+
+                    if (cell == null)
+                    {
+                        dr[i] = null;
+                    }
+                    else
+                    {
+                        dr[i] = cell.ToString();
+                    }
+                }
+                dt.Rows.Add(dr);
+            }
+            
+            return dt;
+        }
+
+    }
+}
