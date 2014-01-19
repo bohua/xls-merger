655e9838c89b320e07e1560ad26dac7f42bbdc6d
bohua <fantasybohua@gmail.com>
2014-01-19 18:31:22 +0100

    Add code

--- xlsMerger/PrintHelper.cs
+++ xlsMerger/PrintHelper.cs
@@ -13,181 +13,221 @@ using System.IO;
 
 namespace XlsMerger
 {
-	class PrintHelper
-	{
-		private List<string> printDocs = new List<string>();
+    class PrintHelper
+    {
+        private List<string> printDocs = new List<string>();
 
-		public void generatePrintDoc(RukuPrintSheet printSheet)
-		{
-			int src, dst;
-			HSSFSheet sheet;
-			FileStream fs = new FileStream(Program.printTemplateRuku, FileMode.Open, FileAccess.ReadWrite);
-			HSSFWorkbook workbook = new HSSFWorkbook(fs);
-			fs.Close();
+        public void generatePrintDoc(RukuPrintSheet printSheet)
+        {
+            int src, dst;
+            HSSFSheet sheet;
+            FileStream fs = new FileStream(Program.printTemplateRuku, FileMode.Open, FileAccess.ReadWrite);
+            HSSFWorkbook workbook = new HSSFWorkbook(fs);
+            fs.Close();
 
-			for (int i = 0; i < (printSheet.getPageSize() + 9); i++)
-			{
-				src = i;
-				dst = printSheet.getPageSize() + 10 + src;
-				sheet = (HSSFSheet)workbook.GetSheetAt(0);
+            for (int i = 0; i < (printSheet.getPageSize() + 9); i++)
+            {
+                src = i;
+                dst = printSheet.getPageSize() + 10 + src;
+                sheet = (HSSFSheet)workbook.GetSheetAt(0);
 
-				CopyRow(workbook, sheet, src, dst);
+                CopyRow(workbook, sheet, src, dst);
 
-				if (src == 0)
-				{
-					sheet.GetRow(dst).HeightInPoints = 25;
-				}
-				else {
-					sheet.GetRow(dst).HeightInPoints = 18;
-				}
-
-				
-			}
-
-			fs = new FileStream(Program.printDoc, FileMode.Create);
-
-			// save the changes
-			workbook.Write(fs);
-			fs.Close();
-		}
-
-		public void PrintMyExcelFile(RukuPrintSheet printSheet)
-		{
+                if (src == 0)
+                {
+                    sheet.GetRow(dst).HeightInPoints = 25;
+                }
+                else
+                {
+                    sheet.GetRow(dst).HeightInPoints = 18;
+                }
 
 
-			Application excelApp = new Application();
-			Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+            }
 
-			// Open the Workbook:
-			Workbook wb = excelApp.Workbooks.Open(
-				Program.printTemplateRuku,
-				Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing,
-				Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing,
-				Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+            fs = new FileStream(Program.printDoc, FileMode.Create);
 
-			// Get the first worksheet.
-			// (Excel uses base 1 indexing, not base 0.)
-			Worksheet ws = (Worksheet)wb.Worksheets[1];
+            // save the changes
+            workbook.Write(fs);
+            fs.Close();
+        }
 
-			// Print out 1 copy to the default printer:
-			ws.PrintOut(
-				Type.Missing, Type.Missing, Type.Missing, Type.Missing,
-				Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+        private void generatePrintPage(RukuPrintSheet printSheet, int pageNum, HSSFWorkbook workbook)
+        {
+            int src, dst;
+            int pageSize = printSheet.getPageSize();
+            HSSFSheet sheet;
+            List<Ruku> printRecords = printSheet.getRecords().GetRange(pageSize * pageNum, pageSize);
 
-			// Cleanup:
-			GC.Collect();
-			GC.WaitForPendingFinalizers();
+            for (int i = 0; i < (printSheet.getPageSize() + 9); i++)
+            {
+                src = i;
+                dst = (printSheet.getPageSize() + 10) * pageNum + src;
+                sheet = (HSSFSheet)workbook.GetSheetAt(0);
 
-			Marshal.FinalReleaseComObject(ws);
+                CopyRow(workbook, sheet, src, dst);
 
-			wb.Close(false, Type.Missing, Type.Missing);
-			Marshal.FinalReleaseComObject(wb);
+                //如果是数据行
+                if (i > 5 && i <= i + pageSize)
+                {
+                    IRow row = sheet.GetRow(pageSize * pageSize + i);
+                    row.GetCell(0).SetCellValue(i - 5);
+                    row.GetCell(1).SetCellValue(printRecords[i - 6].rk_wzmc);
+                    row.GetCell(2).SetCellValue(printRecords[i - 6].rk_ggxh);
+                    row.GetCell(3).SetCellValue(printRecords[i - 6].rk_dw);
+                    row.GetCell(4).SetCellValue(printRecords[i - 6].rk_jhsl);
+                    row.GetCell(5).SetCellValue(printRecords[i - 6].rk_jhdj);
+                    row.GetCell(6).SetCellValue(printRecords[i - 6].rk_jhje);
+                }
 
-			excelApp.Quit();
-			Marshal.FinalReleaseComObject(excelApp);
-		}
+                if (src == 0)
+                {
+                    sheet.GetRow(dst).HeightInPoints = 25;
+                }
+                else
+                {
+                    sheet.GetRow(dst).HeightInPoints = 18;
+                }
+            }
+        }
 
-		/// <summary>
-		/// HSSFRow Copy Command
-		///
-		/// Description:  Inserts a existing row into a new row, will automatically push down
-		///               any existing rows.  Copy is done cell by cell and supports, and the
-		///               command tries to copy all properties available (style, merged cells, values, etc...)
-		/// </summary>
-		/// <param name="workbook">Workbook containing the worksheet that will be changed</param>
-		/// <param name="worksheet">WorkSheet containing rows to be copied</param>
-		/// <param name="sourceRowNum">Source Row Number</param>
-		/// <param name="destinationRowNum">Destination Row Number</param>
-		private void CopyRow(HSSFWorkbook workbook, HSSFSheet worksheet, int sourceRowNum, int destinationRowNum)
-		{
-			// Get the source / new row
-			IRow newRow = worksheet.GetRow(destinationRowNum);
-			IRow sourceRow = worksheet.GetRow(sourceRowNum);
+        public void PrintMyExcelFile(RukuPrintSheet printSheet)
+        {
 
-			// If the row exist in destination, push down all rows by 1 else create a new row
-			if (newRow != null)
-			{
-				worksheet.ShiftRows(destinationRowNum, worksheet.LastRowNum, 1);
-			}
-			else
-			{
-				newRow = worksheet.CreateRow(destinationRowNum);
-			}
 
-			// Loop through source columns to add to new row
-			for (int i = 0; i < sourceRow.LastCellNum; i++)
-			{
-				// Grab a copy of the old/new cell
-				ICell oldCell = sourceRow.GetCell(i);
-				ICell newCell = newRow.CreateCell(i);
+            Application excelApp = new Application();
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
 
-				// If the old cell is null jump to next cell
-				if (oldCell == null)
-				{
-					newCell = null;
-					continue;
-				}
+            // Open the Workbook:
+            Workbook wb = excelApp.Workbooks.Open(
+                Program.printTemplateRuku,
+                Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                Type.Missing, Type.Missing, Type.Missing, Type.Missing);
 
-				// Copy style from old cell and apply to new cell
-				ICellStyle newCellStyle = workbook.CreateCellStyle();
-				newCellStyle.CloneStyleFrom(oldCell.CellStyle); ;
-				newCell.CellStyle = newCellStyle;
+            // Get the first worksheet.
+            // (Excel uses base 1 indexing, not base 0.)
+            Worksheet ws = (Worksheet)wb.Worksheets[1];
 
-				// If there is a cell comment, copy
-				if (newCell.CellComment != null) newCell.CellComment = oldCell.CellComment;
+            // Print out 1 copy to the default printer:
+            ws.PrintOut(
+                Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                Type.Missing, Type.Missing, Type.Missing, Type.Missing);
 
-				// If there is a cell hyperlink, copy
-				if (oldCell.Hyperlink != null) newCell.Hyperlink = oldCell.Hyperlink;
+            // Cleanup:
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
 
-				// Set the cell data type
-				newCell.SetCellType(oldCell.CellType);
+            Marshal.FinalReleaseComObject(ws);
 
-				// Set the cell data value
-				switch (oldCell.CellType)
-				{
-					case CellType.BLANK:
-						newCell.SetCellValue(oldCell.StringCellValue);
-						break;
-					case CellType.BOOLEAN:
-						newCell.SetCellValue(oldCell.BooleanCellValue);
-						break;
-					case CellType.ERROR:
-						newCell.SetCellErrorValue(oldCell.ErrorCellValue);
-						break;
-					case CellType.FORMULA:
-						newCell.SetCellFormula(oldCell.CellFormula);
-						break;
-					case CellType.NUMERIC:
-						newCell.SetCellValue(oldCell.NumericCellValue);
-						break;
-					case CellType.STRING:
-						newCell.SetCellValue(oldCell.RichStringCellValue);
-						break;
-					case CellType.Unknown:
-						newCell.SetCellValue(oldCell.StringCellValue);
-						break;
-				}
-			}
+            wb.Close(false, Type.Missing, Type.Missing);
+            Marshal.FinalReleaseComObject(wb);
 
-			/*
-			// If there are are any merged regions in the source row, copy to new row
-			for (int i = 0; i < worksheet.NumMergedRegions; i++)
-			{
-				CellRangeAddress cellRangeAddress = worksheet.GetMergedRegion(i);
-				if (cellRangeAddress.FirstRow == sourceRow.RowNum)
-				{
-					CellRangeAddress newCellRangeAddress = new CellRangeAddress(newRow.RowNum,
-																				(newRow.RowNum +
-																				 (cellRangeAddress.FirstRow -
-																				  cellRangeAddress.LastRow)),
-																				cellRangeAddress.FirstColumn,
-																				cellRangeAddress.LastColumn);
-					worksheet.AddMergedRegion(newCellRangeAddress);
-				}
-			}
-			 */
+            excelApp.Quit();
+            Marshal.FinalReleaseComObject(excelApp);
+        }
 
-		}
+        /// <summary>
+        /// HSSFRow Copy Command
+        ///
+        /// Description:  Inserts a existing row into a new row, will automatically push down
+        ///               any existing rows.  Copy is done cell by cell and supports, and the
+        ///               command tries to copy all properties available (style, merged cells, values, etc...)
+        /// </summary>
+        /// <param name="workbook">Workbook containing the worksheet that will be changed</param>
+        /// <param name="worksheet">WorkSheet containing rows to be copied</param>
+        /// <param name="sourceRowNum">Source Row Number</param>
+        /// <param name="destinationRowNum">Destination Row Number</param>
+        private void CopyRow(HSSFWorkbook workbook, HSSFSheet worksheet, int sourceRowNum, int destinationRowNum)
+        {
+            // Get the source / new row
+            IRow newRow = worksheet.GetRow(destinationRowNum);
+            IRow sourceRow = worksheet.GetRow(sourceRowNum);
 
-	}
+            // If the row exist in destination, push down all rows by 1 else create a new row
+            if (newRow != null)
+            {
+                worksheet.ShiftRows(destinationRowNum, worksheet.LastRowNum, 1);
+            }
+            else
+            {
+                newRow = worksheet.CreateRow(destinationRowNum);
+            }
+
+            // Loop through source columns to add to new row
+            for (int i = 0; i < sourceRow.LastCellNum; i++)
+            {
+                // Grab a copy of the old/new cell
+                ICell oldCell = sourceRow.GetCell(i);
+                ICell newCell = newRow.CreateCell(i);
+
+                // If the old cell is null jump to next cell
+                if (oldCell == null)
+                {
+                    newCell = null;
+                    continue;
+                }
+
+                // Copy style from old cell and apply to new cell
+                ICellStyle newCellStyle = workbook.CreateCellStyle();
+                newCellStyle.CloneStyleFrom(oldCell.CellStyle); ;
+                newCell.CellStyle = newCellStyle;
+
+                // If there is a cell comment, copy
+                if (newCell.CellComment != null) newCell.CellComment = oldCell.CellComment;
+
+                // If there is a cell hyperlink, copy
+                if (oldCell.Hyperlink != null) newCell.Hyperlink = oldCell.Hyperlink;
+
+                // Set the cell data type
+                newCell.SetCellType(oldCell.CellType);
+
+                // Set the cell data value
+                switch (oldCell.CellType)
+                {
+                    case CellType.Blank:
+                        newCell.SetCellValue(oldCell.StringCellValue);
+                        break;
+                    case CellType.Boolean:
+                        newCell.SetCellValue(oldCell.BooleanCellValue);
+                        break;
+                    case CellType.Error:
+                        newCell.SetCellErrorValue(oldCell.ErrorCellValue);
+                        break;
+                    case CellType.Formula:
+                        newCell.SetCellFormula(oldCell.CellFormula);
+                        break;
+                    case CellType.Numeric:
+                        newCell.SetCellValue(oldCell.NumericCellValue);
+                        break;
+                    case CellType.String:
+                        newCell.SetCellValue(oldCell.RichStringCellValue);
+                        break;
+                    case CellType.Unknown:
+                        newCell.SetCellValue(oldCell.StringCellValue);
+                        break;
+                }
+            }
+
+            /*
+            // If there are are any merged regions in the source row, copy to new row
+            for (int i = 0; i < worksheet.NumMergedRegions; i++)
+            {
+                CellRangeAddress cellRangeAddress = worksheet.GetMergedRegion(i);
+                if (cellRangeAddress.FirstRow == sourceRow.RowNum)
+                {
+                    CellRangeAddress newCellRangeAddress = new CellRangeAddress(newRow.RowNum,
+                                                                                (newRow.RowNum +
+                                                                                 (cellRangeAddress.FirstRow -
+                                                                                  cellRangeAddress.LastRow)),
+                                                                                cellRangeAddress.FirstColumn,
+                                                                                cellRangeAddress.LastColumn);
+                    worksheet.AddMergedRegion(newCellRangeAddress);
+                }
+            }
+             */
+
+        }
+
+    }
 }
